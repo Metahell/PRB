cf59f0429c5a9be64645c2dc3cd86575e03eccb4
Novanef <trios.timothe@hotmail.fr>
2021-05-05 17:05:47 +0200

    Update Kmeans.py

--- Kmeans.py
+++ Kmeans.py
@@ -1,1 +1,71 @@
-import sklearn
+# Import `datasets` from `sklearn`
+from sklearn import datasets
+import numpy as np
+
+# Load in the `digits` data
+digits = datasets.load_digits()
+
+# Print the `digits` data 
+#print(digits)
+
+# Isolate the `digits` data
+digits_data = digits.data
+
+# Inspect the shape
+print(digits_data.shape)
+
+# Isolate the target values with `target`
+digits_target = digits.target
+
+# Inspect the shape
+print(digits_target.shape)
+
+# Print the number of unique labels
+number_digits = len(np.unique(digits.target))
+
+# Isolate the `images`
+digits_images = digits.images
+
+# Inspect the shape
+print(digits_images.shape)
+
+# Import matplotlib
+import matplotlib.pyplot as plt
+
+# Figure size (width, height) in inches
+fig = plt.figure(figsize=(6, 6))
+
+# Adjust the subplots 
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+
+# For each of the 64 images
+for i in range(64):
+    # Initialize the subplots: add a subplot in the grid of 8 by 8, at the i+1-th position
+    ax = fig.add_subplot(8, 8, i + 1, xticks=[], yticks=[])
+    # Display an image at the i-th position
+    ax.imshow(digits.images[i], cmap=plt.cm.binary, interpolation='nearest')
+    # label the image with the target value
+    ax.text(0, 7, str(digits.target[i]))
+
+# Show the plot
+plt.show()
+##
+# Import matplotlib
+import matplotlib.pyplot as plt 
+
+# Join the images and target labels in a list
+images_and_labels = list(zip(digits.images, digits.target))
+
+# for every element in the list
+for index, (image, label) in enumerate(images_and_labels[:8]):
+    # initialize a subplot of 2X4 at the i+1-th position
+    plt.subplot(2, 4, index + 1)
+    # Don't plot any axes
+    plt.axis('off')
+    # Display images in all subplots 
+    plt.imshow(image, cmap=plt.cm.gray_r,interpolation='nearest')
+    # Add a title to each subplot
+    plt.title('Training: ' + str(label))
+
+# Show the plot
+plt.show() 
